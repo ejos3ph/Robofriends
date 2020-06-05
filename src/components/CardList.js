4885e0d7fb309d65d916070557720c15768932f8
ejos3ph <17875832+ejos3ph@users.noreply.github.com>
2020-06-04 20:05:53 -0600

    Populate the list from contacts

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../components/Card';
+
+const CardList=({robots}) =>{
+	return(
+		<div>
+		{			
+			robots.map((user, idx) => {
+			return <Card 
+				key={idx}
+				id={robots[idx].id} 
+				name = {robots[idx].name} 
+				email = {robots[idx].email}
+			/>
+			})
+		}
+		</div>
+	);
+}
+export default CardList
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './components/Card';
+import CardList from './components/CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
 
 ReactDOM.render(
-  
   	<div>
-    	<Card />
-    	<Card />
-    	<Card />
+    	<CardList robots = {robots}/>
   	</div>
   , document.getElementById('root')
 );
