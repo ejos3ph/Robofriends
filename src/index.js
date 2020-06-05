8e43b216a79b65e02867e4c0590c1aa2547f38ee
ejos3ph <17875832+ejos3ph@users.noreply.github.com>
2020-06-04 19:22:12 -0600

    Install tachyons, robot image in place

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Card />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+  	<div>
+    	<Card />
+    	<Card />
+    	<Card />
+  	</div>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
